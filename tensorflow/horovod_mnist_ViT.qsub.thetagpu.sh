da55bc3092a65d487534aba2d41076a568176022
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2021-10-05 23:33:50 +0000

    cleaning up ViT implementation

--- tensorflow/horovod_mnist_ViT.qsub.thetagpu.sh
+++ tensorflow/horovod_mnist_ViT.qsub.thetagpu.sh
@@ -12,11 +12,11 @@ module list
 echo python = $(which python)
 
 NODES=`cat $COBALT_NODEFILE | wc -l`
-#GPUS_PER_NODE=8
-GPUS_PER_NODE=1
+GPUS_PER_NODE=8
+#GPUS_PER_NODE=1
 RANKS=$((NODES * GPUS_PER_NODE))
 echo NODES=$NODES  PPN=$PPN  RANKS=$RANKS
 
 export OMP_NUM_THREADS=16
 
-mpirun -hostfile $COBALT_NODEFILE -n $RANKS -npernode $GPUS_PER_NODE -- python horovod_mnist.py -i /lus/theta-fs0/software/datascience/datasets/mnist.npz --horovod --interop $OMP_NUM_THREADS --intraop $OMP_NUM_THREADS
+mpirun -hostfile $COBALT_NODEFILE -n $RANKS -npernode $GPUS_PER_NODE -- python horovod_mnist_ViT.py -i /lus/theta-fs0/software/datascience/datasets/mnist.npz --horovod --interop $OMP_NUM_THREADS --intraop $OMP_NUM_THREADS

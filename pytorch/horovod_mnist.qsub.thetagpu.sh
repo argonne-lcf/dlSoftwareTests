5bef1b07c8f4d9475168ccc8356b3b0eaf7028d1
Kyle Gerard Felker <felker@anl.gov>
2022-06-09 14:29:51 -0500

    Fix ThetaGPU submission scripts

---
+++ pytorch/horovod_mnist.qsub.thetagpu.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -l
+#COBALT -n 1
+#COBALT -t 60
+#COBALT -q full-node
+#COBALT -A datascience
+
+module load conda/2021-09-2
+conda activate
+
+#MNIST_URL=https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz
+#if [ ! -f "mnist.npz" ]; then
+#   wget $MNIST_URL
+#fi
+
+NODES=$(cat $COBALT_NODEFILE | wc -l)
+GPUS_PER_NODE=8
+RANKS=$((NODES * GPUS_PER_NODE))
+echo NODES=$NODES  GPUS_PER_NODE=$GPUS_PER_NODE  RANKS=$RANKS
+
+export OMP_NUM_THREADS=16
+
+
+echo mpirun -x LD_LIBRARY_PATH -x PATH -x PYTHONPATH -np ${RANKS} -npernode ${GPUS_PER_NODE} --hostfile $COBALT_NODEFILE -- python ./horovod_mnist.py
+echo $PWD
+mpirun -x LD_LIBRARY_PATH -x PATH -x PYTHONPATH -np ${RANKS} -npernode ${GPUS_PER_NODE} --hostfile $COBALT_NODEFILE -- python ./horovod_mnist.py
+
+echo test mpi4py
+mpirun -x LD_LIBRARY_PATH -x PATH -x PYTHONPATH -np ${RANKS} -npernode ${GPUS_PER_NODE} --hostfile $COBALT_NODEFILE -- python -c "import mpi4py.MPI as mpi;import socket;print('host: ',socket.gethostname(), 'rank: ',mpi.COMM_WORLD.Get_rank(),' size: ',mpi.COMM_WORLD.Get_size())"
--- tensorflow/horovod_imagenet_ViT.qsub.thetagpu.sh
+++ tensorflow/horovod_imagenet_ViT.qsub.thetagpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#COBALT -n 1 
+#COBALT -n 1
 #COBALT -t 480
 #COBALT -q full-node
 #COBALT -A datascience
@@ -16,7 +16,7 @@ echo python = $(which python)
 NODES=`cat $COBALT_NODEFILE | wc -l`
 GPUS_PER_NODE=8
 RANKS=$((NODES * GPUS_PER_NODE))
-echo NODES=$NODES  PPN=$PPN  RANKS=$RANKS
+echo NODES=$NODES  GPUS_PER_NODE=$GPUS_PER_NODE  RANKS=$RANKS
 
 export OMP_NUM_THREADS=16
 
@@ -24,6 +24,4 @@ export OMP_NUM_THREADS=16
 #mpirun -x LD_LIBRARY_PATH -x PATH -x PYTHONPATH -np $RANKS -npernode $GPUS_PER_NODE --hostfile $COBALT_NODEFILE python horovod_mnist_ViT.py -i /lus/theta-fs0/software/datascience/datasets/mnist.npz --horovod --interop $OMP_NUM_THREADS --intraop $OMP_NUM_THREADS
 
 #Original - works on 1 node
-mpirun -x LD_LIBRARY_PATH -x PATH -x PYTHONPATH -np $((COBALT_JOBSIZE*8)) -npernode 8 --hostfile ${COBALT_NODEFILE} python horovod_imagenet_ViT.py -i /lus/theta-fs0/software/datascience/datasets/mnist.npz --horovod --interop $OMP_NUM_THREADS --intraop $OMP_NUM_THREADS
-
-#mpirun -hostfile $COBALT_NODEFILE -n $RANKS -npernode $GPUS_PER_NODE -- python horovod_mnist_ViT.py -i /lus/theta-fs0/software/datascience/datasets/mnist.npz --horovod --interop $OMP_NUM_THREADS --intraop $OMP_NUM_THREADS
+mpirun -x LD_LIBRARY_PATH -x PATH -x PYTHONPATH -np ${RANKS} -npernode ${GPUS_PER_NODE} --hostfile ${COBALT_NODEFILE} python horovod_imagenet_ViT.py -i /lus/theta-fs0/software/datascience/datasets/mnist.npz --horovod --interop $OMP_NUM_THREADS --intraop $OMP_NUM_THREADS
